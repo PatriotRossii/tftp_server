8782f3daad75a73cd9fb8caae8d7cb39075b8f97
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-15 20:33:39 +0300

    Add tftp_common 1.2.0 as a dependency

--- tftp_server/main.cpp
+++ tftp_server/main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 
-#include <tftp_common/packets.hpp>
-#include <tftp_common/serialization.hpp>
+#include <tftp_common/tftp_common.hpp>
 
+#include <iostream>
+#include <cstdlib>
+
+using namespace tftp_common::packets;
 using boost::asio::ip::udp;
 
 int main() {
@@ -14,17 +15,14 @@ int main() {
 		udp::socket socket(io_context, udp::endpoint(udp::v4(), 69));
 
 		for (;;) {
-			boost::array<char, 1024> recv_buf;
+			boost::array<std::uint8_t, 1024> recv_buf;
 			udp::endpoint remote_endpoint;
 			socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
 
-			std::vector<char> send_buffer;
-			tftp_common::packets::error err = {
-				.ErrorCode = 0,
-				.ErrMsg = "UNIMPLEMENTED"
-			};
-			err.serialize(std::back_inserter(send_buffer));
-			socket.send_to(boost::asio::buffer(send_buffer), remote_endpoint);
+			std::vector<std::uint8_t> send_buffer(1024);
+			Error err(10, "UNIMPLEMENTED");
+			std::size_t packetSize = err.serialize(send_buffer.begin());
+			socket.send_to(boost::asio::buffer(send_buffer, packetSize), remote_endpoint);
 		}
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
